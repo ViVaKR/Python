40ef9eebf6fa36c46fde62a380dca61d8023e285
ViVaKR <admin@vivabj.com>
2023-04-08 20:57:26 +0900

    Add dictionary

---
+++ main.py
@@ -0,0 +1,13 @@
+a = 1
+print(type(a)) 
+
+b = 3.141593
+print(type(b))
+
+c = 3.14e10         # == 3.14 * 10 ^ 10
+print(type(c), c)   # == <type 'float'>, 31400000000.0
+
+a = 3
+b = 4
+print(a * b)
+
