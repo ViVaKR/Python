595b5a000da7d71a44ae92262cbf3572d960797c
Kim Bum Jun <admin@vivabj.com>
2024-11-17 11:23:59 +0900

    Add new np.ipynb

--- Viv/np.py
+++ Viv/np.py
@@ -1,4 +1,10 @@
 import numpy as np
 
-print(np.array([1, 2, 3, 4, 5]))
+arr_1 = np.array([1, 2, 3, 4, 5])
+print(np.array(arr_1))
 
+
+
+
+# python -m pip install --upgrade pip
+# pip install numpy
---
+++ Viv/run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# np.py 파일 실행
+python np.py
